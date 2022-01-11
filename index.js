023cdb802866f2ff39ca0295c1b0d4352daeffee
Mohammed Zubair <mohammedzubair10@yahoo.com>
2022-01-11 09:54:25 -0500

    commit tree 85bed0e1e72307f4aba11d22fddfea69d4de7cc3

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  
+  if(someValue <= 400) {
+    return "This one is on me!"
+ } else if (someValue > 2500) {
+    return "No can do."
+  } else if (someValue > 2000) {
+    return "I will gladly take your thirty bucks."
+  } 
 }
+  
+function ternaryCheckCity(city){
+  return city === "NYC" ? 'Ok, sounds good.' : 'No go.';
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(money){
+  switch(money) {
+    case 'generous':
+        return 'Thank you so much.'
+    case 'not as generous':
+        return 'Thank you.'
+    case 'thanks for everything':
+        return 'Bye.'
+  } 
+  
 }
